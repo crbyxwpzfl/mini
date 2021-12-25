bf22aa89c68d040b09c5b8b755b90bfda6f1210a
f <socials.fl@icloud.com>
2021-12-25 15:12:03 +0100

    ignore errors

--- pullreadlist.py
+++ pullreadlist.py
@@ -8,6 +8,7 @@ import youtube_dl
 
 ydl_opts = {
     'restrict-filenames': True,
+    'ignoreerrors:' True,
     'download_archive': '/Users/mini/Desktop/archive.txt',
     'outtmpl': '/Users/mini/Desktop/%(title)s.%(ext)s',
 }
