87e5739808b311d49506a3ecf1191e5e3ac4b3ec
f <socials.fl@icloud.com>
2021-12-25 14:52:38 +0100

    test treeee

--- pullreadlist.py
+++ pullreadlist.py
@@ -23,14 +23,31 @@ def my_hook(d):
 
 
 ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
+    'restrict-filenames':
     'logger': MyLogger(),
     'progress_hooks': [my_hook],
 }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
+
+
+
+import re
+import sys
+import subprocess
+
+output = subprocess.Popen(['plutil', '-convert', 'xml1', '-o', '~/Desktop/SafariBookmarks.xml', '~/Library/Safari/Bookmarks.plist'], stdout=subprocess.PIPE)
+
+
+#os.system('plutil -convert xml1 -o ~/Desktop/SafariBookmarks.xml ~/Library/Safari/Bookmarks.plist')
+
+
+
+file = open("/Users/mini/Desktop/SafariBookmarks.xml", "r")
+
+for line in file:
+	if re.search("^					<string>http", line):
+		print (line[13:-10])
+		url = line[13:-10]
+		ytdlpath = "/Library/Frameworks/Python.framework/Versions/3.9/bin/youtube-dl"
+		output = subprocess.Popen([ytdlpath, '--restrict-filenames', '-o', '/Users/mini/Desktop/%(title)s.%(ext)s', url, '--no-continue', '--no-check-certificate', '--download-archive', 'archive.txt'], stdout=subprocess.PIPE)
