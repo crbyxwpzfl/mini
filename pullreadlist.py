f71239b8d6289e902883fe4c76757d89884a19bb
f <socials.fl@icloud.com>
2021-12-25 14:46:30 +0100

    test uno

--- pullreadlist.py
+++ pullreadlist.py
@@ -1,7 +1,36 @@
 import sys
 
-
 sys.path.append("/usr/local/bin/youtube-dl")
+
+from __future__ import unicode_literals
 import youtube_dl
 
-b
+
+class MyLogger(object):
+    def debug(self, msg):
+        print(msg)
+
+    def warning(self, msg):
+        print(msg)
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
