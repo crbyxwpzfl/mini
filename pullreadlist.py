b76bc1a5c25ccbd65386a15d097a01749763f9ea
f <socials.fl@icloud.com>
2021-12-25 15:05:55 +0100

    no log

--- pullreadlist.py
+++ pullreadlist.py
@@ -6,28 +6,9 @@ sys.path.append("/usr/local/bin/youtube-dl")
 import youtube_dl
 
 
-class MyLogger(object):
-    def debug(self, msg):
-        print(msg)
-
-    def warning(self, msg):
-        print(msg)
-
-    def error(self, msg):
-        print(msg)
-
-
-def my_hook(d):
-    if d['status'] == 'finished':
-        print('Done downloading, now converting ...')
-
-
 ydl_opts = {
     'restrict-filenames': True,
     'outtmpl': '/Users/mini/Desktop/%(title)s.%(ext)s',
-
-    'logger': MyLogger(),
-    'progress_hooks': [my_hook],
 }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
