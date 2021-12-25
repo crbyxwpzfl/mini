d47fed5558cf81a15d15d951e901cae37296dea0
f <socials.fl@icloud.com>
2021-12-25 16:14:50 +0100

    try logging

--- pullreadlist.py
+++ pullreadlist.py
@@ -14,7 +14,12 @@ ydl_opts = {
     'ignoreerrors': True,
     'download_archive': '/Users/mini/Desktop/archive.txt',
     'outtmpl': '/Users/mini/Desktop/%(title)s.%(ext)s',
+    'logger': MyLogger(),
 }
+
+class MyLogger(object):
+    print (msg)
+
 
 #convert bookmark plist to xml
 output = subprocess.Popen(['plutil', '-convert', 'xml1', '-o', '/Users/mini/Desktop/SafariBookmarks.xml', '/Users/mini/Library/Safari/Bookmarks.plist'], stdout=subprocess.PIPE)
