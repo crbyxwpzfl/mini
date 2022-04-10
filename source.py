89be6a4df83dc3370922aa2c6f95954879535a61
f <socials.fl@icloud.com>
2022-04-10 20:37:56 +0200

    lid open tv on hdm2

--- source.py
+++ source.py
@@ -10,7 +10,7 @@ import requests
 from requests.auth import HTTPDigestAuth
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
+import time
 
 characteristic = sys.argv[3].strip("''")
 
@@ -32,6 +32,12 @@ if '"AppleClamshellState" = Yes' in out:
 
 #lid auf
 elif '"AppleClamshellState" = No' in out:
+
+    data = '{key: "Standby"}'
+    
+    response = requests.post(f'https://{privates.ip}:1926/6/input/key', timeout=2, data=data, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
+
+    time.sleep(2)
 
     data = {'intent': {'extras': {'query': 'hdmi 2'}, 'action': 'Intent {  act=android.intent.action.ASSIST cmp=com.google.android.katniss/com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline flg=0x10200000 }', 'component': {'packageName': 'com.google.android.katniss', 'className': 'com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline'}}}
 
