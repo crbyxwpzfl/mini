03f26681e347982f19243675fc508579d89c88aa
f <socials.fl@icloud.com>
2022-04-11 12:50:33 +0200

    refactor pretty code

--- source.py
+++ source.py
@@ -1,71 +1,70 @@
 #import privates variable
 import sys
-import os
-#sys.path.append(os.environ.get('privates'))
 sys.path.append('/Users/mini/Downloads/private/')
 import privates
 
-import subprocess
-import requests
+#ip = privates.ip
+#user = privates.user
+#pw = privates.pw
+#minipath = privates.minipath
+
+import os
 from requests.auth import HTTPDigestAuth
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import requests
 import time
+import subprocess
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # disable http warnings
 
-#asume tv is off
-status = 0
 
-characteristic = sys.argv[3].strip("''")
 
-if characteristic == "name":
-    print("test")
+
+
+def onoffstate():
+    response = requests.get(f'https://{privates.ip}:1926/6/powerstate', verify=False, timeout=2, auth=HTTPDigestAuth(privates.user, privates.pw))
+    if "On" in str(response.content):
+        return 1
+
+def hdmi(data):
+    with open(os.path.join(privates.minipath, 'source.txt'), "r+") as f: # open file as read and wirte
+        if data != "read":  # if data read do not wirte
+            f.write(data)   # write data
+            f.truncate()    # delete rest of file
+        f.seek(0)           # go to beniging of file               
+        try:
+            return f.read() # return read hold back by try 
+        finally:
+            f.close()       # finally closes file then releases return  
+
+def post(path, data):
+    response = requests.post(f'https://{privates.ip}:1926/6/{path}', timeout=2, json=data, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
+
+
+
+
+if sys.argv[3].strip("''") == "name": # rturns name and stops script
+    print("mini")
     sys.exit()
 
-#check if tv is off if tv is on status = 1
-response = requests.get(f'https://{privates.ip}:1926/6/powerstate', verify=False, timeout=2, auth=HTTPDigestAuth(privates.user, privates.pw))
-if "On" in str(response.content):
-    status = 1
-
-#detect lid status
-output = subprocess.Popen(['ioreg', '-r', '-k', 'AppleClamshellState'], stdout=subprocess.PIPE)
-out = str(output.stdout.read())
-
-#read current hdmi state from source.txt
-f = open(os.path.join(privates.minipath, 'source.txt'), 'r')
-hdmi = int(f.read())
-f.close()
-
-#lid zu
-if '"AppleClamshellState" = Yes' in out:
-
-    #if in hdmi2 turn tv off and wirte hdmi 1 to source.txt
-    if int(hdmi) == 2:
-        data = '{key: "Standby"}'
-        response = requests.post(f'https://{privates.ip}:1926/6/input/key', timeout=2, data=data, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
-        
-        f = open(os.path.join(privates.minipath, 'source.txt'), 'w')
-        f.write(1)
-        f.close()
+output = subprocess.Popen(['ioreg', '-r', '-k', 'AppleClamshellState'], stdout=subprocess.PIPE) # gets lid state
+if '"AppleClamshellState" = Yes' in str(output.stdout.read()): # if lid closed
+    
+    if int(hdmi("read")) == 2 : # if hdmi2 switch to hdmi1 and turn tv off and write hdmi1 in sourcetxt
+        post("activities/launch", {'intent': {'extras': {'query': 'hdmi 1'}, 'action': 'Intent {  act=android.intent.action.ASSIST cmp=com.google.android.katniss/com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline flg=0x10200000 }', 'component': {'packageName': 'com.google.android.katniss', 'className': 'com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline'}}})
+        time.sleep(4)
+        post("input/key", {'key': 'Standby'})
+        hdmi("1")
     
     print("OCCUPANCY_NOT_DETECTED")
 
-#lid auf
-if '"AppleClamshellState" = No' in out:
+if '"AppleClamshellState" = Yes' in str(output.stdout.read()): # if lid open 
     
-    #ad detection for source state
-    #only switch source if in hdmi1 
-    if int(status) == 0:
-        data = '{key: "Standby"}'
-        response = requests.post(f'https://{privates.ip}:1926/6/input/key', timeout=2, data=data, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
+    if onoffstate() == None: # if tv off turn tv on
+        post("input/key", {'key': 'Standby'})
+        time.sleep(4)
 
-    if int(hdmi) == 1:
-        time.sleep(2)
-        data = {'intent': {'extras': {'query': 'hdmi 2'}, 'action': 'Intent {  act=android.intent.action.ASSIST cmp=com.google.android.katniss/com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline flg=0x10200000 }', 'component': {'packageName': 'com.google.android.katniss', 'className': 'com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline'}}}
-        response = requests.post(f'https://{privates.ip}:1926/6/activities/launch', timeout=2, json=data, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
-
-        f = open(os.path.join(privates.minipath, 'source.txt'), 'w')
-        f.write(2)
-        f.close()
+    if int(hdmi("read")) == 1: # if sourcetxt hdmi1 switch to hdmi2 
+        post("activities/launch", {'intent': {'extras': {'query': 'hdmi 2'}, 'action': 'Intent {  act=android.intent.action.ASSIST cmp=com.google.android.katniss/com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline flg=0x10200000 }', 'component': {'packageName': 'com.google.android.katniss', 'className': 'com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline'}}})
+        hdmi("2")
     
-    print("OCCUPANCY_DETECTED")    
-    sys.exit()
+    print("OCCUPANCY_DETECTED")
