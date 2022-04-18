a414860fe7146d1b065deee3a24247483bdb7afa
f <socials.fl@icloud.com>
2022-04-18 17:55:04 +0200

    interim while refactor to omit txts

--- ambilight.py
+++ ambilight.py
@@ -8,6 +8,8 @@ import sys
 sys.path.append('/Users/mini/Downloads/private/')
 import privates
 from requests.auth import HTTPDigestAuth
+import json
+import colorsys
 import requests
 import math
 import os        
@@ -19,18 +21,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # disable ht
 
 # support fuctions
 
-def onoffstate():
-    try:
-        response = requests.get(f'https://{privates.ip}:1926/6/powerstate', verify=False, timeout=2, auth=HTTPDigestAuth(privates.user, privates.pw))
-    except requests.exceptions.ConnectionError:
-        print("  ----  error connecting getting powerstate  ----  ")
-        sys.exit()
-    except requests.exceptions.Timeout:
-        print("  ----  timeout error getting powerstate  ----  ")
-        sys.exit()
-    else:
-        if "On" in str(response.content):
-            return 1
+
 
 def rw(data, txt):
     with open(os.path.join(privates.minipath, txt), "r+") as f: # open file as read and wirte
@@ -42,6 +33,10 @@ def rw(data, txt):
             return f.read() # return read hold back by try 
         finally:
             f.close()       # finally closes file then releases return  
+
+def get(path):
+    response = requests.get(f'https://{privates.ip}:1926/6/{path}', timeout=2, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
+    return response.content 
 
 def post(body):
     try:
@@ -59,6 +54,7 @@ def convert():
     v = float(rw("read", "Brightness.txt"))/100
     
 
+
     if s == 0.0: v*=255; r, g, b = v, v, v
     i = int(h*6.) # XXX assume int() truncates!
     f = (h*6.)-i; p,q,t = int(255*(v*(1.-s))), int(255*(v*(1.-s*f))), int(255*(v*(1.-s*(1.-f)))); v*=255; i%=6
@@ -72,13 +68,46 @@ def convert():
     return f"{{r: {int(r)}, g: {int(g)}, b: {int(b)}}}"
 
 
+#convert rgb to hsv
+
+def rgbtohsv(r, g, b):
+    (h, s, v) = colorsys.rgb_to_hsv(r/255, g/255, b/255)
+    return(int(h*360), int(s*100), int(v*100))
+
+def hsvtorgb(h, s, v):
+    (r, g, b) = colorsys.hsv_to_rgb(((h-7)%360)/360, math.pow((s/100),0.5), v/100)
+    print(int(r*255), int(g*255), int(b*255))
 
 
 # logic and output
 
 if sys.argv[1] == "Get":
-    print(rw("read", str(sys.argv[3].strip("''"))+'.txt'), end='')
+    
+    #powerstate
+    if json.loads(get('powerstate'))['powerstate'] == 'Standby':
+        print("0", end='') 
+    else: 
+        print('1', end='')
     sys.exit()
+
+    #other
+def test():
+    rgb = json.loads(get('ambilight/cached'))['layer1']['left']['0']
+    (Hue, Saturation, Brightness) = rgbtohsv(rgb['r'], rgb['g'], rgb['b'])
+    print(locals()[str(sys.argv[1].strip("''"))])
+    sys.exit()
+
+
+    rgb = json.loads(get('ambilight/cached'))['layer1']['left']['0']
+    d = {}
+    (d['Hue'], d['Saturation'], d['Brightness']) = rgbtohsv(rgb['r'], rgb['g'], rgb['b'])
+
+    print(d)
+
+    #print(locals()[str(sys.argv[1].strip("''"))])
+
+    print(d[sys.argv[1]])
+
 
 
 if sys.argv[1] == "Set":   
