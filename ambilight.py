e08a0fdf24d217b0c96dd985ab84ffcf91dc1e3a
f <socials.fl@icloud.com>
2022-04-24 17:46:32 +0200

    kown issues

--- ambilight.py
+++ ambilight.py
@@ -77,7 +77,7 @@ def calculatehsv():
     d.update({'Hue': int(h*360),'Saturation': int(s*100),'Brightness': int(v*100)})
 
 def calculatergb():
-    (r, g, b) = colorsys.hsv_to_rgb(((d['Hue']-7)%360)/360, math.pow((d['Saturation']/100),0.5), (d['Brightness']+1)/100)
+    (r, g, b) = colorsys.hsv_to_rgb(((d['Hue']-7)%360)/360, math.pow((d['Saturation']/100),0.5), (d['Brightness'])/100)
     d.update({'r': int(r*255),'g': int(g*255),'b': int(b*255)})
 
 def calculateon():
@@ -90,7 +90,8 @@ def calculateon():
 d = {'Set': Set, 'Get': Get, 'On': calculateon, 'Hue': hsv,'Saturation': hsv,'Brightness': hsv}
 d.get(sys.argv[1].strip("''"))()
 
-
+# KNOWN ISSUES
+# no initial start values meaning to get going one need to go to full brightness
 
 
 
