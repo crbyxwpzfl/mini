1e901921c07dc35db9863e102d886ad693cfbd85
f <socials.fl@icloud.com>
2022-04-24 13:03:12 +0200

    in the mids of refactoring

--- ambilight.py
+++ ambilight.py
@@ -21,9 +21,20 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # disable ht
 
 # support fuctions
 
-def get(path):
-    response = requests.get(f'https://{privates.ip}:1926/6/{path}', timeout=2, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
-    return response.content
+def gethsv():
+    response = requests.get(f'https://{privates.ip}:1926/6/ambilight/cached', timeout=2, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
+    return hsvof(json.loads(response.content)['layer1']['left']['0']) #returns dict with 'Hue' 'Saturation' 'Brightness'
+    
+
+def geton():
+    response = requests.get(f'https://{privates.ip}:1926/6/powerstate', timeout=2, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
+    return {json.loads(response.content)['powerstate']: 1} #returns dict with 'On' or 'Standby'
+
+
+#def get(path):
+#    response = requests.get(f'https://{privates.ip}:1926/6/{path}', timeout=2, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
+#    return response.content
+
 
 def post(body):
     try:
@@ -36,9 +47,11 @@ def post(body):
         sys.exit()
 
 
-def hsvof(r, g, b):
-    (h, s, v) = colorsys.rgb_to_hsv(r/255, g/255, b/255)
-    return(int(h*360), int(s*100), int(v*100))
+def hsvof(rgb):
+    (h, s, v) = colorsys.rgb_to_hsv(rgb['r']/255, rgb['g']/255, rgb['b']/255)
+    return {'Hue': int(h*360),'Saturation': int(s*100),'Brightness': int(v*100)}
+    
+    #return(int(h*360), int(s*100), int(v*100))
 
 def rgbof(h, s, v):
     (r, g, b) = colorsys.hsv_to_rgb(((h-7)%360)/360, math.pow((s/100),0.5), (v+1)/100)
@@ -50,28 +63,33 @@ def rgbof(h, s, v):
 # logic and output
 
 if sys.argv[1] == "Get":
-    if str(sys.argv[3].strip("''")) == "On":
-        d = {'On': 0}   #creates dict and assumes off state
-        d[json.loads(get('powerstate'))['powerstate']] = 1 #overwrites key On or creates key Standby
-    else:
-        d = json.loads(get('ambilight/cached'))['layer1']['left']['0'] #creates dict with keys r g b
-        (d['Hue'], d['Saturation'], d['Brightness']) = hsvof(d['r'], d['g'], d['b']) #adds keys Hue Saturation Brightness 
-    
-    print(d[sys.argv[3].strip("''")])   #returns value of wanted characteristic  
-    sys.exit()    
+    d = {'On': geton} # creates dict with 'On'
+    d = d.get(sys.argv[3].strip("''"), gethsv)() #overwrites dict with 'Hue' 'Saturation' 'Brightness' or 'Standby' or 'On'
+    print(d.get(sys.argv[3].strip("''"), 0))   #returns value of 'characteristic' or 1 
 
 
-if sys.argv[1] == "Set" :
 
-    #ACHTUNG this is shit redoo this!!!
-
-    d = {'On': 1}   #creates dict and assumes on state
-    d[json.loads(get('powerstate'))['powerstate']] = 0 #overwrites key On or creates key Standby
+d = {'On': geton} # creates dict with 'On'
+d = d.get(sys.argv[3].strip("''"), gethsv)() #overwrites dict with 'Hue' 'Saturation' 'Brightness' or 'Standby' or 'On'
+if sys.argv[1] == "Set" and geton()['Standby'] == 1: #only if tv is off
     
 
-    if d['On'] == 1: # only if tv is off
-        d.update(json.loads(get('ambilight/cached'))['layer1']['left']['0']) #adds keys r g b
-        (d['Hue'], d['Saturation'], d['Brightness']) = hsvof(d['r'], d['g'], d['b']) #adds keys Hue Saturation Brightness 
-        d[sys.argv[3].strip("''")] = int(sys.argv[4].strip("''"))    #update value of wanted characteristic
-        (d['r'], d['g'], d['b']) = rgbof(d['Hue'], d['Saturation'], d['Brightness']) #update r g b values
-        post({'r': d['r']*d['On'], 'g': d['g']*['On'], 'b': d['b']*d['On']}) #multiplie with on state to turn light off
+
+    d = geton() #creates dict with 'On' or 'Standby'
+    if d.get('On') == None: # only if tv is off
+        d.update(gethsv()) #adds 'Hue' 'Saturation' 'Brightness'
+        d[sys.argv[3].strip("''")] = int(sys.argv[4].strip("''"))    #update value of 'characteristic' or creates 'On' with value
+        (d['r'], d['g'], d['b']) = rgbof(d['Hue'], d['Saturation'], d['Brightness']) #update 'r' 'g' 'b' values
+        post({'r': d['r']*d['On'], 'g': d['g']*d['On'], 'b': d['b']*d['On']}) #multiplie with 'on' to turn lights off
+
+
+#    d = {json.loads(get('powerstate'))['powerstate']: 1} #creates dict with 'On' or 'Standby'
+#    if d.get('On') == None: # only if tv is off
+#        d.update(json.loads(get('ambilight/cached'))['layer1']['left']['0']) #adds 'r' 'g' 'b'
+#        (d['Hue'], d['Saturation'], d['Brightness']) = hsvof(d['r'], d['g'], d['b']) #adds 'Hue' 'Saturation' 'Brightness' 
+#        d[sys.argv[3].strip("''")] = int(sys.argv[4].strip("''"))    #update value of 'characteristic' or creates 'On' with value
+#        (d['r'], d['g'], d['b']) = rgbof(d['Hue'], d['Saturation'], d['Brightness']) #update 'r' 'g' 'b' values
+#        post({'r': d['r']*d['On'], 'g': d['g']*d['On'], 'b': d['b']*d['On']}) #multiplie with 'on' to turn lights off
+
+    
+    if d.get('On') == 1: # only if tv is on
