77ebd1b12cdcad618d3448a0017ab7a9e0b166f6
f <socials.fl@icloud.com>
2022-04-25 11:04:22 +0200

    close to done refactoring

--- ambilight.py
+++ ambilight.py
@@ -12,33 +12,21 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # disable http warnings
 
 def Get():
-    d.get(sys.argv[3].strip("''"))()
-    print(d.get(sys.argv[3].strip("''")))
+    reference()
+    print(d.get(sys.argv[3].strip("''"), int(d['Brightness']/d['Brightness']) if d['Brightness'] else 0 ))    # prints 'characteristic' or calculates 'On'
 
 def Set():
     response = requests.get(f'https://{privates.ip}:1926/6/powerstate', timeout=2, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
-    hsv() if json.loads(response.content)['powerstate'] == 'Standby' else sys.exit()    #only when standby
-    d[sys.argv[3].strip("''")] = int(sys.argv[4].strip("''")) #update value of 'characteristic'
-    calculatergb()
-    if d['r'] == 0 and d['g'] == 0 and d['b'] == 0 and d['Brightness'] != 0: d['r'] = 1 #inital value issue for hsv conversion
-    response = requests.post(f'http://{privates.ip}:1925/6/ambilight/cached', timeout=2, json={'r': d['r'],'g': d['g'],'b': d['b']})
+    reference() if json.loads(response.content)['powerstate'] == 'Standby' and sys.argv[3].strip("''") != 'On' else sys.exit()    # only when standby and ignore 'On' so only 'Brightness' causes on/offs
+    d[sys.argv[3].strip("''")] = int(sys.argv[4].strip("''"))    # update 'characteristic' value
+    (r, g, b) = colorsys.hsv_to_rgb(((d['Hue']-7)%360)/360, math.pow((d['Saturation']/100),0.5), (d['Brightness'])/100)    # including Hue shift and Saturation boost
+    response = requests.post(f'http://{privates.ip}:1925/6/ambilight/cached', timeout=2, json={'r': int(r*255),'g': int(g*255),'b': int(b*255)})
 
-def hsv():
+def reference():
     response = requests.get(f'https://{privates.ip}:1926/6/ambilight/cached', timeout=2, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
-    d.update( json.loads(response.content)['layer1']['left']['0'] ) #updates 'r' 'g' 'b'
-    calculatehsv() #updates 'Hue' 'Saturation' 'Brightness'
+    d.update( json.loads(response.content)['layer1']['left']['0'] )    # adds 'r' 'g' 'b'
+    (h, s, v) = colorsys.rgb_to_hsv(d['r']/255, d['g']/255, d['b']/255)    
+    d.update({'Hue': int(h*360),'Saturation': int(s*100),'Brightness': int(v*100)})    # adds 'Hue' 'Saturation' 'Brightness'
 
-def calculatehsv():
-    (h, s, v) = colorsys.rgb_to_hsv(d['r']/255, d['g']/255, d['b']/255)
-    d.update({'Hue': int(h*360),'Saturation': int(s*100),'Brightness': int(v*100)})
-
-def calculatergb():
-    (r, g, b) = colorsys.hsv_to_rgb(((d['Hue']-7)%360)/360, math.pow((d['Saturation']/100),0.5), (d['Brightness'])/100) #including Hue shift and Saturation boost
-    d.update({'r': int(r*255),'g': int(g*255),'b': int(b*255)})
-
-def calculateon():
-    hsv()
-    d.update({'On': int(d[Brightness]/d[Brightness]) if d['Brightness'] else 0})
-
-d = {'Set': Set, 'Get': Get, 'On': calculateon, 'Hue': hsv,'Saturation': hsv,'Brightness': hsv}
+d = {'Set': Set, 'Get': Get}
 d.get(sys.argv[1].strip("''"))()
