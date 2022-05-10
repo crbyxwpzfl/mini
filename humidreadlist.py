233be2b9b3d56cd30656746218e7140fc4276b8a
f <socials.fl@icloud.com>
2022-05-10 17:36:06 +0200

    bit mor cleaning up

--- humidreadlist.py
+++ humidreadlist.py
@@ -7,7 +7,6 @@ import os
 import subprocess
 import fileinput # for overwritesite()
 
-
 def Get():
     print(dict.get(sys.argv[3].strip("''") , 1))
     sys.exit()
@@ -17,14 +16,17 @@ def run(cmdstring): # string here because shell true because only way of chaning
     #print(lines.stdout)
     return lines.stdout
 
+def vpnstatus():
+    fuckedlist = run(d['sshpi'] + "nordvpn status")[12:].strip('\n').split('\n') # get vpn status and clean up output a bit
+    for count, elem in enumerate(fuckedlist): d[fuckedlist[count].split(': ')[0]] = fuckedlist[count].split(':')[1].strip() # for each elem split elem by : then add first elem of split as key and second as value to dict
+
 def overwritesite():
-    fuckedlist = run(d['sshpi'] + "nordvpn status")[12:].strip('\n').split('\n') # clean up output and add to dict
-    for count, elem in enumerate(fuckedlist): d[fuckedlist[count].split(': ')[0]] = fuckedlist[count].split(':')[1].strip() # for each elem split elem by : then add first elem of split as key and second as value
-    d['ccode'] = d.get('Current server', "de")[:2] # insert tihs into calss selector
-    d['color'] = "#5cf287" if d['Status'] == 'Connected' else "#fc4444" # insert this into color
-    d['line7'] = f"path.{d['ccode']} {{fill: {d['color']};}}  /* set color and ccode according to on off state */\n"
+    vpnstatus()
+    d['ccode'] = d.get('Current server', "de")[:2] # get country then insert country code into css calss selector
+    d['color'] = "#5cf287" if d['Status'] == 'Connected' else "#fc4444" # get on off color insert color part of css class selector
+    d['line7'] = f"path.{d['ccode']} {{fill: {d['color']};}}  /* set color and ccode according to on off state */\n" # construct linnes
     d['line8'] = f"path.{d['ccode']}:hover {{stroke: {d['color']}; stroke-width: 4; fill: {d['color']};}}\n"
-    for line in fileinput.input(d['site'], inplace=True):
+    for line in fileinput.input(d['site'], inplace=True): # open file and overwrite lines
         print(d['line7'], end='') if fileinput.filelineno() == 7 else print(d['line8'], end='') if fileinput.filelineno() == 8 else print(line, end='')
 
 def pushsite():
@@ -32,8 +34,7 @@ def pushsite():
         run(d['gitcssh'] + f" -C {os.path.join(d['puthere'], 'reposetories')} clone git@github.com:crbyxwpzfl/{r}.git") # TODO move this to setup function
         run(d['gitcssh'] + f" -C {os.path.join(d['puthere'], 'reposetories', r)} pull") # gets changes from remote add --quiet to shut up
         d['message']+= r + " "
-    #if all up to date
-    #change file now
+    overwritesite() # update site content
     run(f"git -C {os.path.join(d['puthere'], 'reposetories', 'spinala')} commit -am \"site update\" ; " + d['gitcssh'] + f" -C {os.path.join(d['puthere'], 'reposetories', 'spinala')} push" ) # commit -am does not picup on new created files
     run(f"osascript -e tell application \"Messages\" to send \"site updated and pulled {message}\" to participant \"{d['phonenr']}\"") # send message site updated
 
