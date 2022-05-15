a3842db4655c0c9b48ef7e3b860b4fcebf86670c
f <socials.fl@icloud.com>
2022-05-15 14:09:42 +0200

    swithing from run to popen and testing starts again

--- humidreadlist.py
+++ humidreadlist.py
@@ -19,10 +19,10 @@ import time # for mess()
 
 def pluses(): # TODO debug
     for r in d['repos']: # out of pushsite() TODO only pull spinala here rest perhaps in a complete back up funktion
-        run(d['gitcssh'] + f" -C {os.path.join(d['puthere'], 'reposetories')} clone git@github.com:crbyxwpzfl/{r}.git") # TODO move this to setup function
+        sub(d['gitcssh'] + f" -C {os.path.join(d['puthere'], 'reposetories')} clone git@github.com:crbyxwpzfl/{r}.git", True) # TODO move this to setup function
         print(f"cloned {r} to {os.path.join(d['puthere'], 'reposetories')}")
     
-    run(f"plutil -convert xml1 -o {os.path.join(d['puthere'], 'reposetories', 'ff', 'SafariBookmarks.xml')} {os.path.join(os.environ.get('HOME'), 'Library', 'Safari', 'Bookmarks.plist')}") # out of parsereadlist() TODO move this to setup function
+    sub(f"plutil -convert xml1 -o {os.path.join(d['puthere'], 'reposetories', 'ff', 'SafariBookmarks.xml')} {os.path.join(os.environ.get('HOME'), 'Library', 'Safari', 'Bookmarks.plist')}", True) # out of parsereadlist() TODO move this to setup function
 
     # individualy working
     parsereadlist() # waht u want vpn location and urls
@@ -34,7 +34,7 @@ def pluses(): # TODO debug
     print(f"curr serv is [:2] - {d.get('Current server', 'where u are')[:2]}")
 
     if d.get('vpnto', "what u want wh")[-2:] != d.get('Current server', "where u are")[:2]: 
-        run(d['sshpi']  + "nordvpn " + d.get('vpnto', "disconnect"))
+        sub(d['sshpi']  + "nordvpn " + d.get('vpnto', "disconnect"), True)
         print("sshed to change vpn")
     else:
         print("did no ssh")
@@ -50,35 +50,42 @@ def pluses(): # TODO debug
     print("")
     print(d['Status'])
 
-    if d.get('vpnto', "what u want wh")[-2:] == d.get('Current server', "where u are")[:2] and d['Status'] == "Connected" and d['dlpurls'] and len(run("killall -s Python").split('kill')) == 2:  # +1 account for list.split always len 1 and +1 for Python currently running so this means if no dlp is up
-        run(f"osascript -e 'tell app \"Terminal\" to do script \"{pathlib.Path(__file__).resolve()} dlp\" ' ") # call itself and bring dlp() up in new window
+    if d.get('vpnto', "what u want wh")[-2:] == d.get('Current server', "where u are")[:2] and d['Status'] == "Connected" and d['dlpurls'] and len(sub("killall -s Python", True).split('kill')) == 2:  # +1 account for list.split always len 1 and +1 for Python currently running so this means if no dlp is up
+        # TODO remove run(f"osascript -e 'tell app \"Terminal\" to do script \"{pathlib.Path(__file__).resolve()} dlp\" ' ") # call itself and bring dlp() up in new window
+        sub(f"osascript -e 'tell app \"Terminal\" to do script \"{pathlib.Path(__file__).resolve()} dlp\" ' ", False) # dont wait until completion call itself and bring dlp() up in new window
         print("started dlp")
     else:
         print("did not start dlp()")
 
     print(d['ariaurls'])
 
-    if len(run("killall -s aria2c").split('kill')) == 2 and d.get('Uptime', 'shiiit') == "shiiit": # prolly should not happen but yeah
+    if len(sub("killall -s aria2c").split('kill'), True) == 2 and d.get('Uptime', 'shiiit') == "shiiit": # prolly should not happen but yeah
         sendaria( {'jsonrpc': '2.0', 'id': 'mini', 'method': 'aria2.shutdown'} )
-        run(f"osascript -e 'tell application \"Messages\" to send \"aria on vpn off\" to participant \"{d['phonenr']}\"'")
+        sub(f"osascript -e 'tell application \"Messages\" to send \"aria on vpn off\" to participant \"{d['phonenr']}\"'", True)
+
+
 
     # TODO review new message system and keep debuging especially aria
     # TODO implement in hb and backup config!!
 
-def run(cmdstring): # TODO replace this by sub
-    process = subprocess.run(cmdstring , text=False, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    print(process.stdout.decode()) # TODO make programm quiet
-    return process.stdout.decode()
+#def run(cmdstring): # TODO replace this by sub
+#    process = subprocess.run(cmdstring , text=False, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+#    print(process.stdout.decode()) # TODO make programm quiet
+#    return process.stdout.decode()
 
-def sub(cmdstring): # string here because shell true because only way of chaning commands
+def sub(cmdstring, waitforcompletion): # string here because shell true because only way of chaning commands
     d['sub'] = subprocess.Popen(cmdstring , text=False, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    #print(d['sub'].communicate()[0].decode()) # this will wait for p to finisih
+    if wait: 
+        print d['sub'].communicate()[0].decode() # TODO remove this to shutup
+        return d['sub'].communicate()[0].decode() # this will wait for subprocess to finisih
 
 def mess(message, title):
     sub(f"osascript -e 'display notification \"{message}\" with title \"{title}\"'")
     # TODO implement    sub("osascript -e 'tell app \"Terminal\"' -e ' do script \"qlmanage -p /Users/mini/Desktop/\"' -e 'set W to the id of window 1' -e 'set visible of window 1 to false' -e 'do script \"curl -s \\\"http://localhost:8080/motion?screen\\\" && exit \"' -e 'delay 2' -e 'close window id W' -e 'end tell'")
     # TODO implement    sub("osascript -e 'tell app \"Safari\"' -e 'open location \"https://crbyxwpzfl.github.io/spinala/\"' -e 'delay 2' -e 'close (current tab of window 1)' -e 'end tell'")
     # TODO implement    sub("osascript -e 'tell app \"System Events\"' -e 'keystroke space using {control down}' -e 'delay 0.5' -e 'keystroke the \"message\"' -e 'delay 0.5' -e 'keystroke space using {control down}' -e 'end tell'")
+    # TODO implement    sub("osascript -e 'tell app \"Messages\"' -e 'activate' -e 'delay 1' -e 'end tell' -e 'tell application \"System Events\"' -e 'keystroke the \"https://crbyxwpzfl.github.io/spinala/\"' -e 'delay 1' -e 'keystroke return' -e 'delay 2' -e 'keystroke \"q\" using {command down}' -e 'end tell'", False)
+
 
 def parsereadlist(): # when foldername not in downloaddir add url to aria or dlp dict
     plist = plistlib.load(open(os.path.join(os.environ.get('HOME'), 'Library', 'Safari', 'Bookmarks.plist'), 'rb'))
@@ -91,7 +98,8 @@ def parsereadlist(): # when foldername not in downloaddir add url to aria or dlp
                 if item['URIDictionary']['title'].startswith('push vpn to '): d['vpnto'] = "connect " + item['URLString'][-2:] # vpn to country into d 'vpnto'
 
 def vpnstate(): # pipe vpn status into dict
-    nicelist = run(d['sshpi'] + "nordvpn status").lstrip('\r-\r  \r\r-\r  \r').rstrip('\n').split('\n') # get vpn status and clean up output a bit
+    # TODO remove nicelist = run(d['sshpi'] + "nordvpn status").lstrip('\r-\r  \r\r-\r  \r').rstrip('\n').split('\n') # get vpn status and clean up output a bit
+    nicelist = sub(d['sshpi'] + "nordvpn status", True).lstrip('\r-\r  \r\r-\r  \r').rstrip('\n').split('\n') # get vpn status and clean up output a bit
     for count, elem in enumerate(nicelist): d[nicelist[count].split(': ')[0]] = nicelist[count].split(':')[1].strip() # for each elem split elem by : then add first elem of split as key and second as value to dict
 
 def overwritesite(): # overwrite site content corrosponding to parsereadlist() not vpnstate()
@@ -102,26 +110,28 @@ def overwritesite(): # overwrite site content corrosponding to parsereadlist() n
         print(d['line52'], end='') if fileinput.filelineno() == 52 else print(d['line56'], end='') if fileinput.filelineno() == 56 else print(line, end='')
 
 def pushsite(): # pull all repos and push changes of overwritesite()
-    run(d['gitcssh'] + f" -C {os.path.join(d['puthere'], 'reposetories', 'spinala')} pull") # TODO gets changes from remote add --quiet to shut up 
+    # TODO remove run(d['gitcssh'] + f" -C {os.path.join(d['puthere'], 'reposetories', 'spinala')} pull")
+    sub(d['gitcssh'] + f" -C {os.path.join(d['puthere'], 'reposetories', 'spinala')} pull", True) # TODO gets changes from remote add --quiet to shut up 
     overwritesite() # update site content
-    run(f"git -C {os.path.join(d['puthere'], 'reposetories', 'spinala')} commit -am \"site update\" ; " + d['gitcssh'] + f" -C {os.path.join(d['puthere'], 'reposetories', 'spinala')} push ;" ) # commit -am does not picup on newly created files
-    run(f"osascript -e 'tell application \"Messages\" to send \"{d.get('vpnto', 'off')} https://crbyxwpzfl.github.io/spinala/\" to participant \"{d['phonenr']}\"'") # TODO perhaps remove send message on site updated
-    mess("tell app \"Safari\"", "-e 'make new document with properties {URL:\"https://crbyxwpzfl.github.io/spinala/\"}' -e 'return id of window 1' -e'end tell'")
+    # TODO remove run(f"git -C {os.path.join(d['puthere'], 'reposetories', 'spinala')} commit -am \"site update\" ; " + d['gitcssh'] + f" -C {os.path.join(d['puthere'], 'reposetories', 'spinala')} push ;" )
+    sub(f"git -C {os.path.join(d['puthere'], 'reposetories', 'spinala')} commit -am \"site update\" ; " + d['gitcssh'] + f" -C {os.path.join(d['puthere'], 'reposetories', 'spinala')} push ;", True) # commit -am does not picup on newly created files
+    # TODO remove run(f"osascript -e 'tell application \"Messages\" to send \"{d.get('vpnto', 'off')} https://crbyxwpzfl.github.io/spinala/\" to participant \"{d['phonenr']}\"'")
+    sub(f"osascript -e 'tell application \"Messages\" to send \"{d.get('vpnto', 'off')}\" to participant \"{d['phonenr']}\"'", False)
+    sub("osascript -e 'tell app \"Messages\"' -e 'activate' -e 'delay 1' -e 'end tell' -e 'tell application \"System Events\"' -e 'keystroke the \"https://crbyxwpzfl.github.io/spinala/\"' -e 'delay 1' -e 'keystroke return' -e 'delay 2' -e 'keystroke \"q\" using {command down}' -e 'end tell'", False) # dont wait use this so link preview loads nicely
 
 def dlp(): # TODO perhaps use internal merge/convert tool with ffmpeg to generate mp4 and use archive at d['puthere']/repos/ff/dwl-archive
     parsereadlist() # to get desired urls now in new process here head() and paresreadlist never got called
     for url in d['dlpurls']:
         d['dlpopts']['outtmpl'] = os.path.join(d['puthere'], url[0], 'filename-vc:%(vcodec)s-ac:%(acodec)s.%(ext)s') # the first item in each url list is the foldername
         with yt_dlp.YoutubeDL(d['dlpopts']) as ydl: ydl.download(url[1]) # the second item in each url list is the url
-        #run(f"osascript -e 'tell application \"Messages\" to send \"dlp done {url[0]}\" to participant \"{d['phonenr']}\"'") # TODO remove
-        d['message'] = f"d['message'] echo {url[1]} && " # here unlike aria() chained messages
-    mess("tell app \"Terminal\"", f"-e 'do script \"echo done && {d['message']} echo && du -hs {d['puthere']}*\"' -e 'end tell'")
+        sub(f"osascript -e 'display notification \"done {url[1]}\" with title \"dlp\"'", False) # dont wait on completion just fire notification
     os.kill(os.getppid(), signal.SIGHUP) # close window when done
 
 def sendaria(data):
         try: d['r'] = requests.post('http://localhost:6800/jsonrpc', json=data, verify=False, timeout=2)
         except requests.exceptions.ConnectionError: 
-            if d['Status'] == "Connected": run(f"aria2c {d['ariaopts']}") # error connecting so aria is off so start aria so no added url so url stays in queue so addes url next time
+            if d['Status'] == "Connected": sub(f"aria2c {d['ariaopts']}", True) # wait until completion # # error connecting so aria is off so start aria so no added url so url stays in queue so addes url next time
+                                          #TODO remove run(f"aria2c {d['ariaopts']}")
 
 def aria(): # TODO perhaps use more advanced opts add trackers and optimize concurrent downloads and save savefile every sec or so
     for url in d['ariaurls']: # on download completion call this bitsh empty so yeeet    smae for if not d['ariaurls'] at shutdown purge send message
@@ -131,33 +141,35 @@ def aria(): # TODO perhaps use more advanced opts add trackers and optimize conc
     for stopped in json.loads(d['r'].content)['result'][1][0]: # man im numb all this nested list dict shit braeks me here we want the first list in the second list in r content result list
         d['message'] = f"{stopped.get('status')} {stopped.get('errorMessage')[:80]}" # make message
         for fs in stopped.get('files', [{'path':'nofile'}]):
-            d['message'] = f"{fs.get('path')} {d['message']}" # here unlike dlp() no chained messages
-            #if not d['ariaurls']: run(f"osascript -e 'tell application \"Messages\" to send \"aria {d['message']}\" to participant \"{d['phonenr']}\"'") # TODO remove message and implement motion call
-            if not d['ariaurls']: mess("tell app \"Terminal\"", f"-e 'do script \"echo {d['message']} echo && du -hs {d['puthere']}*\"' -e 'end tell'")
-    #if not d['ariaurls']: sendaria( {'jsonrpc': '2.0', 'id': 'mini', 'method': 'aria2.purgeDownloadResulti'} ) # TODO no purge to keep history of errors  purge aria so next message is clean shuld be save and shuld not make me miss anything
+            d['message'] = f"{fs.get('path')} {d['message']}"
+            if not d['ariaurls']: sub(f"osascript -e 'display notification \"done {d['message']}\" with title \"aria\"'", False) # dont wait on completion just fire notification # only on aria completion call so when no parsing happend so ther is no d['ariaurls']
+            # TODO remove if not d['ariaurls']: mess("tell app \"Terminal\"", f"-e 'do script \"echo {d['message']} echo && du -hs {d['puthere']}*\"' -e 'end tell'")
+    if not d['ariaurls']: sendaria( {'jsonrpc': '2.0', 'id': 'mini', 'method': 'aria2.purgeDownloadResulti'} ) # TODO no purge to keep history of errors  purge aria so next message is clean shuld be save and shuld not make me miss anything
     if not d['ariaurls'] and d['CurrentRelativeHumidity'] == 0: sendaria( {'jsonrpc': '2.0', 'id': 'mini', 'method': 'aria2.shutdown'} ) #if no active and no waiting in queue shutdown aria 
 
 def head():
     parsereadlist() # waht u want vpn location and urls
-    vpnstate() # where u are
-    if d.get('vpnto', "what u want wh")[-2:] != d.get('Current server', "where u are")[:2]: run(d['sshpi']  + "nordvpn " + d.get('vpnto', "disconnect")) # only set vpn when parsereadlist() vpn state not current vpnstate()
+    vpnstate() # where u are                                                                TODO remove run(d['sshpi']  + "nordvpn " + d.get('vpnto', "disconnect"))
+    if d.get('vpnto', "what u want wh")[-2:] != d.get('Current server', "where u are")[:2]: sub(d['sshpi']  + "nordvpn " + d.get('vpnto', "disconnect"), True) # only set vpn when parsereadlist() vpn state not current vpnstate()
     if d.get('vpnto', "what u want wh")[-2:] != d.get('Current server', "where u are")[:2]: pushsite() # only push site when parsereadlist() vpnstate not current vpnstate(). pushsite() itself sets site corrosponding to parsereadlist() not vpnstate()
-
-    if len(run("killall -s aria2c").split('kill')) == 2 and d.get('Uptime', 'shiiit') == "shiiit": # prolly should not happen but yeah
+           
+           # TODO remove run("killall -s aria2c").split('kill')
+    if len(sub("killall -s aria2c").split('kill'), True) == 2 and d.get('Uptime', 'shiiit') == "shiiit": # prolly should not happen but yeah
         sendaria( {'jsonrpc': '2.0', 'id': 'mini', 'method': 'aria2.shutdown'} )
-        run(f"osascript -e 'tell application \"Messages\" to send \"aria on vpn off\" to participant \"{d['phonenr']}\"'")
+        sub(f"osascript -e 'tell application \"Messages\" to send \"aria on vpn off\" to participant \"{d['phonenr']}\"'", True)
 
     if d.get('vpnto', "what u want wh")[-2:] == d.get('Current server', "where u are")[:2] and d['Status'] == "Connected" and d['ariaurls']: # dont do aria() when parsereadlist() vpn state not vpnstate() eg off not stil on cause vpnstate() is lagging actual vpn is off and bunch others
         aria()
-
-    if d.get('vpnto', "what u want wh")[-2:] == d.get('Current server', "where u are")[:2] and d['Status'] == "Connected" and d['dlpurls'] and len(run("killall -s Python").split('kill')) == 2:  # +1 account for list.split always len 1 and +1 for Python currently running so this means if no dlp is up
-        run(f"osascript -e 'tell app \"Terminal\" to do script \"{pathlib.Path(__file__).resolve()} dlp\" ' ") # call itself and bring dlp() up in new window
-
+                                                                                                                                                # TODO remove run("killall -s Python").split('kill')
+    if d.get('vpnto', "what u want wh")[-2:] == d.get('Current server', "where u are")[:2] and d['Status'] == "Connected" and d['dlpurls'] and len(sub("killall -s Python", True).split('kill')) == 2:  # +1 account for list.split always len 1 and +1 for Python currently running so this means if no dlp is up
+        # TODO remove run(f"osascript -e 'tell app \"Terminal\" to do script \"{pathlib.Path(__file__).resolve()} dlp\" ' ") # call itself and bring dlp() up in new window
+        sub(f"osascript -e 'tell app \"Terminal\" to do script \"{pathlib.Path(__file__).resolve()} dlp\" ' ", False) # dont wait until completion call itself and bring dlp() up in new window
+    
     print(d.get(sys.argv[3].strip("''") , int(len(d.get('Uptime', ''))/len(d.get('Uptime', '1'))) )) # print aria count to homebridge or print aria on as 'StatusActive' or calculate vpn on as 'StatusTampered' as in location tampered
     sys.exit()
 
 d = {'debug': pluses,'Get': head, 'dlp': dlp, # defs for running directly in cli via arguments
-    'CurrentRelativeHumidity': 80, 'StatusActive': len(run("killall -s aria2c").split('kill'))-1, # for homebridge
+    'CurrentRelativeHumidity': 80, 'StatusActive': len(sub("killall -s aria2c", True).split('kill'))-1, # for homebridge TODO remove run("killall -s aria2c")
     'gitcssh': f"git -c core.sshCommand=\"ssh -i {privates.opensshpriv}\"", # for clone pull psuh
     'sshpi': f"ssh {privates.piaddress} -i {privates.opensshpriv} ", # attentione to the last space
     'repos': ["private", "mini", "ff", "spinala", "rogflow", "crbyxwpzfl"], # all these repos get cloned
