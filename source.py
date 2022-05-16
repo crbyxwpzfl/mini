23b2ae7c5750dc037e4f2f2c955a6b201e4529df
f <socials.fl@icloud.com>
2022-05-16 13:57:00 +0200

    okok looks ok

--- source.py
+++ source.py
@@ -15,15 +15,15 @@ import subprocess
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # disable http warnings
 
-
 def post(path, data):
-    response = requests.post(f'https://{privates.ip}:1926/6/{path}', timeout=2, json=data, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
+    response = requests.post(f'https://{privates.ip}:1926/6/{path}', timeout=2, data=data, verify=False, auth=HTTPDigestAuth(privates.user, privates.pw))
+    print(response.content)
 
 def Get():
     d['tv'] = 1 if "On" in str(requests.get(f'https://{privates.ip}:1926/6/powerstate', verify=False, timeout=2, auth=HTTPDigestAuth(privates.user, privates.pw)).content) else 0
     d['mini'] = 0 if '"AppleClamshellState" = Yes' in  str(subprocess.run(['ioreg', '-r', '-k', 'AppleClamshellState'], stdout=subprocess.PIPE).stdout.decode()) else 1
 
-    if d['mini'] != d['tv']: # switch hdmi turns tv on automaticly
+    if d['mini'] == int(d['hdmi'] - 1) or (d['tv'] == 0 and d['mini'] == 1): # switch hdmi turns tv on automaticly
         post("activities/launch", f"{{'intent': {{'extras': {{'query': 'hdmi {d['hdmi']}'}}, 'action': 'Intent {{  act=android.intent.action.ASSIST cmp=com.google.android.katniss/com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline flg=0x10200000 }}', 'component': {{'packageName': 'com.google.android.katniss', 'className': 'com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline'}} }} }} ")
         
         d['line37'] = f"     'hdmi': {3 - int(d['hdmi'])},\n" # prints 2 if was 1 and 1 if was 2 to 'hdmi'
@@ -34,6 +34,6 @@ def Get():
 
 d = {'Get': Get, # defs for running directly in cli via arguments
      'name': 'mini', # characteristics
-     'hdmi': 1,
+     'hdmi': 2,
     }
 d.get(sys.argv[1].strip("''"), sys.exit)()
