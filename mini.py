7e0b1a6ee68806b9abd3469c471e4040b635f43f
f <socials.fl@icloud.com>
2022-05-16 17:58:52 +0200

    ok interim save

--- mini.py
+++ mini.py
@@ -27,23 +27,8 @@ def primedir(at):
         os.replace(os.path.join(d['puthere'], at, files), os.path.join(clonehere, at, f"tmp-{files}"))
 
 def deltemp(at):
-    for p in Path(os.path.join(clonehere, at)).glob("tmp*"):    #delete tmp dirs
+    for p in Path(os.path.join(d['puthere'], at)).glob("tmp*"):    #delete tmp dirs
         shutil.rmtree(p)
-
-
-def pluses(): # TODO debug
-
-        primedir('gists')
-        response = requests.get('https://api.github.com/users/crbyxwpzfl/gists')    #get all gists
-        for i in response.json():
-            foldername = i.get('description', " ")
-            for items in i['files']: 
-                foldername += i['files'][x]['filename'].replace(".", "-") + " "
-
-            sub(['git', 'clone', i['git_pull_url'], os.path.join(clonehere, 'gists', foldername)])    #add [, '--quiet'] to shut up    
-        deltemp('gists')
-        sub(['osascript', '-e', f'tell application "Messages" to send "cloned gists" to participant "{phonenr}"'])
-
 
 def clone():
     sub(f"plutil -convert xml1 -o {os.path.join(d['puthere'], 'transfer', 'reposetories', 'ff', 'bookmarks.xml')} {os.path.join(os.environ.get('HOME'), 'Library', 'Safari', 'Bookmarks.plist')}") # puts bookmarks into ff
@@ -51,38 +36,20 @@ def clone():
 
     for r in d['repos']: # clone repos
         sub(d['gitcssh'] + f" -C {os.path.join(d['puthere'], 'reposetories')} clone git@github.com:crbyxwpzfl/{r}.git")
-    
     for r in d['repos']: # pull repos
         sub(d['gitcssh'] + f" -C {os.path.join(d['puthere'],'reposetories', r)} pull") # TODO gets changes from remote add --quiet to shut up 
 
     primedir('gists')
     response = requests.get('https://api.github.com/users/crbyxwpzfl/gists')    #get all gists
 
-    for  in response.json():
-            if i['description']:    #use description as name or else chain file names
-                foldername = i['description']
-            else:
-                foldername = ""
-                for x in i['files']:
-                    foldername += i['files'][x]['filename'].replace(".", "-") + " "
-
-            sub(['git', 'clone', i['git_pull_url'], os.path.join(clonehere, 'gists', foldername)])    #add [, '--quiet'] to shut up
+    for gist in response.json(): # use desription or all filenames as filename
+        foldername = gist.get('description', " ")
+        for f in gist['files']: 
+            foldername += gist['files'][f]['filename'].replace(".", "-") + " "
+        sub(f"git clone {gist['git_pull_url']} {os.path.join(d['puthere'], 'gists', foldername)}") # add --quiet to shut up
     
-        deltemp('gists')
-        sub(['osascript', '-e', f'tell application "Messages" to send "cloned gists" to participant "{phonenr}"'])
-
-    if a in ['cr', '-cr', 'clonerepos', '-clonerepos']:   #slow but works fuck it
-        primedir('reposetories')
-
-        downedrepos = " "
-        for r in repos:
-            sub(['git', '-C', os.path.join(clonehere, 'reposetories'),'-c', f"core.sshCommand=\"\"ssh -i {sshpriv}\"\"", 'clone', f'git@github.com:crbyxwpzfl/{r}.git'])     #add [, '--quiet'] to shut up 
-            downedrepos += r + " "
-  
-        deltemp('reposetories')
-        sub(['osascript', '-e', f'tell application "Messages" to send "pulled {downedrepos}" to participant "{phonenr}"'])
-
-
+    deltemp('gists')
+    sub(f"osascript -e 'tell application \"Messages\" to send \"pushed bookmarks pulled repos and gists\" to participant \"{d['phonenr']}\"'")
 
 
 def convert():
