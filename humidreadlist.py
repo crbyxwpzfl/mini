aff4ee5932a4582ff7ce4152e1f199d5a0492057
f <socials.fl@icloud.com>
2022-06-20 14:00:12 +0200

    cleanup

--- humidreadlist.py
+++ humidreadlist.py
@@ -35,7 +35,7 @@ def mess(message, title):
 
 
 def parsereadlist(): # when foldername not in downloaddir add url to aria or dlp dict
-    listoftupls = sqlite3.connect('/Users/mini/Library/Messages/chat.db').cursor().execute(d['sqlquery']).fetchall() # sql connect make cursor execute query wait for query to finish
+    listoftupls = sqlite3.connect(d['chatdb']).cursor().execute(d['sqlquery']).fetchall() # sql connect make cursor execute query wait for query to finish
     for tupl in listoftupls:
         if '??' in tupl[0] and tupl[0].rsplit('??',1)[1].replace('-', ' ') not in os.listdir(os.path.join(d['puthere'], 'temps')) and tupl[0].startswith('https://'): d['slpurls'].append(tupl[0].rsplit('??',1)) # all https into dlp
         if '??' in tupl[0] and tupl[0].rsplit('??',1)[1].replace('-', ' ') not in os.listdir(os.path.join(d['puthere'], 'temps')) and tupl[0].startswith('http://'): d['ariaurls'].append(tupl[0].strip('http://').rsplit('??',1)) # all http into aria
@@ -116,6 +116,7 @@ d = {'Get': head, 'dlp': dlp, # defs for running directly in cli via arguments
     'dlpurls': [],
     'dlpopts': {'simulate': False, 'restrict-filenames': False, 'ignoreerrors': True, 'format': 'bestvideo*,bestaudio', 'verbos': True, 'external_downloader': {'m3u8': 'aria2c'}},
     'ariaopts': f"--enable-rpc --rpc-listen-all --on-download-complete={pathlib.Path(__file__).resolve()} --save-session=/Users/mini/Desktop/ariasfile.txt --input-file=/Users/mini/Desktop/ariasfile.txt --daemon=false --auto-file-renaming=false --allow-overwrite=false --seed-time=0", # daemon false otherwise no message on completion reason unknown
+    'chatdb': '/Users/mini/Library/Messages/chat.db'
     'sqlquery': 'SELECT message.text FROM message JOIN chat_handle_join ON message.handle_id = chat_handle_join.handle_id JOIN chat ON chat.ROWID = chat_handle_join.chat_id WHERE (chat.chat_identifier="crbyxwpzfl@icloud.com" OR chat.chat_identifier="+4915224066305") ORDER BY message.date desc;'
 }
 d.get(sys.argv[1].strip("''"), aria)() # call head() with 'Get' from homebridge or aria() on download completion of aria only works daemon false remember to not wait for completion on aria start
