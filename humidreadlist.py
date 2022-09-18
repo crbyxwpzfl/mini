0497642304e174d75e45fbf81c4a8f2c23d4674c
f <socials.fl@icloud.com>
2022-09-18 15:11:16 +0200

    pi adress is already beeing leaked in config so fuck it

--- humidreadlist.py
+++ humidreadlist.py
@@ -102,7 +102,7 @@ def head(): # run full head just on 'CurrentRelativeHumidity' to minimize pi que
 
 d = {'get': head, 'dlp': dlp, # defs for running directly in cli via arguments
     'gitcssh': f"git -c core.sshCommand=\"ssh -i {privates.opensshpriv}\"", # for clone pull psuh
-    'sshpi': f"ssh {privates.piaddress} -i {privates.opensshpriv} ", # attentione to the last space
+    'sshpi': f"ssh pi@pi@192.168.2.1 -i {privates.opensshpriv} ", # attentione to the last space
     'puthere': '/Users/mini/Downloads/', # put 'puthere'/transfer/reposetories/spinala for site update and 'puthere'/temps/dwls here
     'phonenr': privates.phone, # for vpn message and sql query
     'ariaurls': [],
