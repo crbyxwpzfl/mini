aaa871a7ba15d92b6b1c2f4cfcb5d6585854ba11
f <socials.fl@icloud.com>
2022-10-10 14:15:56 +0200

    test abspath to config

--- mini.py
+++ mini.py
@@ -3,14 +3,17 @@
 # chmod +x /current/file && cd "/current/" && git config core.filemode true && git commit -am "commit chmodx" && git -c core.sshCommand="ssh -i /path/to/priv" push
 
 import os
+import pathlib
 import subprocess
 import time
 
 def sub(cmdstring, waitforcompletion): # string here because shell true because only way of chaning commands
     p = subprocess.Popen(cmdstring , text=False, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     if waitforcompletion: print(p.communicate()[0].decode()) # this will wait for subprocess to finisih 
-        
-sub("screen -S hb -d -m homebridge",False) #  summon hb
+
+print({os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'config.json')))
+
+sub(f"screen -S hb -d -m homebridge -U {os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'config.json'))} ",False) #  summon hb
 time.sleep(2)
 sub("screen -S vs -d -m code-server serve",False) #  summon vs
 time.sleep(2)
