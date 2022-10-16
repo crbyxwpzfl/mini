daf97a100c85bd86fba1bf37c9ff5ec403c37bfa
f <socials.fl@icloud.com>
2022-10-16 17:35:04 +0200

    hopefully this now works more reliable

--- wall.py
+++ wall.py
@@ -10,7 +10,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # disable ht
 
 def set():  # when wall off switch hdmi also turns wall    when wall on presspowerbutton or set vloume
     if d['Brightness'] == 0 and sys.argv[3] == 'Brightness' and sys.argv[4] != '0':  # switch hdmi to 2 when wall off and volume not 0
-        requests.post(f'https://{secs.wallip}:1926/6/activities/launch',  timeout=2, json={'intent': {'extras': {'query': 'hdmi 1'}, 'action': 'Intent {  act=android.intent.action.ASSIST cmp=com.google.android.katniss/com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline flg=0x10200000 }', 'component': {'packageName': 'com.google.android.katniss', 'className': 'com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline'} } } , verify=False, auth=HTTPDigestAuth(secs.walluser, secs.wallpw))
+        requests.post(f'https://{secs.wallip}:1926/6/activities/launch',  timeout=2, json={'intent': {'extras': {'query': 'HDMI 1'}, 'action': 'Intent {  act=android.intent.action.ASSIST cmp=com.google.android.katniss/com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline flg=0x10200000 }', 'component': {'packageName': 'com.google.android.katniss', 'className': 'com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline'} } } , verify=False, auth=HTTPDigestAuth(secs.walluser, secs.wallpw))
 
     if d['Brightness'] >= 1 and sys.argv[3] == 'Brightness' and sys.argv[4] == '0':  # press power button when wall on and when volume 0
         requests.post(f'https://{secs.wallip}:1926/6/input/key', timeout=2, json={'key': 'Standby'}, verify=False, auth=HTTPDigestAuth(secs.walluser, secs.wallpw))
