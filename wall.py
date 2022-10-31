2f2ac4e972054c60df0b086bf725ecd0c3e6f8dc
f <socials.fl@icloud.com>
2022-10-31 15:16:47 +0100

    little changes to wall

--- wall.py
+++ wall.py
@@ -1,4 +1,4 @@
-# set up alex app on tv so tv stays in light sleep not deep sleep so api is responsive at all times even after long times of inactivety
+# set up alexa app on tv so tv stays in light sleep not deep sleep so api is responsive at all times even after long times of inactivety
 
 import sys; sys.path.append('/Users/mini/Downloads/transfer/reps-privates/'); import secs  # fetch secrets
 
@@ -10,14 +10,13 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # disable ht
 
 def set():  # when wall off switch hdmi also turns wall    when wall on presspowerbutton or set vloume
     if d['Brightness'] == 0 and sys.argv[3] == 'Brightness' and sys.argv[4] != '0':  # switch hdmi to 2 when wall off and volume not 0
-        requests.post(f'https://{secs.wallip}:1926/6/activities/launch',  timeout=2, json={'intent': {'extras': {'query': 'HDMI 1'}, 'action': 'Intent {  act=android.intent.action.ASSIST cmp=com.google.android.katniss/com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline flg=0x10200000 }', 'component': {'packageName': 'com.google.android.katniss', 'className': 'com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline'} } } , verify=False, auth=HTTPDigestAuth(secs.walluser, secs.wallpw))
+        requests.post(f'https://{secs.wallip}:1926/6/activities/launch',  timeout=2, json={'intent': {'extras': {'query': 'HDMI 1'}, 'action': 'Intent {  act=android.intent.action.ASSIST cmp=com.google.android.katniss/com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline flg=0x10200000 }', 'component': {'packageName': 'com.google.android.katniss', 'className': 'com.google.android.apps.tvsearch.app.launch.trampoline.SearchActivityTrampoline'} } } , verify=False, auth=HTTPDigestAuth(secs.walluser, secs.wallpw)); sys.exit()
 
     if d['Brightness'] >= 1 and sys.argv[3] == 'Brightness' and sys.argv[4] == '0':  # press power button when wall on and when volume 0
-        requests.post(f'https://{secs.wallip}:1926/6/input/key', timeout=2, json={'key': 'Standby'}, verify=False, auth=HTTPDigestAuth(secs.walluser, secs.wallpw))
+        requests.post(f'https://{secs.wallip}:1926/6/input/key', timeout=2, json={'key': 'Standby'}, verify=False, auth=HTTPDigestAuth(secs.walluser, secs.wallpw)); sys.exit()
 
     if d['Brightness'] >= 1 and sys.argv[3] == 'Brightness' and sys.argv[4] != '0':  #  set volume when wall on and dont change volume to 0 when shutting off
-        requests.post(f'https://{secs.wallip}:1926/6/audio/volume', timeout=2, json={'muted': 'false', 'current': int(sys.argv[4]) }, verify=False, auth=HTTPDigestAuth(secs.walluser, secs.wallpw))
+        requests.post(f'https://{secs.wallip}:1926/6/audio/volume', timeout=2, json={'muted': 'false', 'current': int(sys.argv[4]) }, verify=False, auth=HTTPDigestAuth(secs.walluser, secs.wallpw)); sys.exit()
 
-d = {'Brightness': json.loads(requests.get(f'https://{secs.wallip}:1926/6/audio/volume', timeout=2, verify=False, auth=HTTPDigestAuth(secs.walluser, secs.wallpw)).content)['current']
-    }
+d = { 'Brightness': json.loads(requests.get(f'https://{secs.wallip}:1926/6/audio/volume', timeout=2, verify=False, auth=HTTPDigestAuth(secs.walluser, secs.wallpw)).content)['current'] }
 print(d.get(sys.argv[3], max( min(d['Brightness'],1), 0) )) if sys.argv[1] == 'Get' else set()  # calls set or prints 'Brightness' or calculates/prints 'On' calculation assumes arc turns volume 0 and powerbutton turns volume 0
