0b0c2c9e85daf7c697fb98cbbc3940b729241ca7
f <socials.fl@icloud.com>
2022-12-05 11:53:25 +0100

    outdir uses title now

--- humidreadlist.py
+++ humidreadlist.py
@@ -44,50 +44,44 @@ def dl():  # NOTE consider --allow-overwrite=true/'overwrite': True, # sysargv2 
         sub(f'aria2c "{sys.argv[2].split("/",3)[3]}" --save-session={os.path.join(os.getcwd(), "ariasfile.txt")} --seed-time=0', True)  # use safefile with --input-file /path/to/ariasfile.txt to resume any stoped downloads
         sort()
 
-# TODO get titel of video for naming dl dir and make this as reliable and as fast as possible
-
-def tapback(message, emote):  # this is inline just for simplyfinging edits for futur ui changes (like/2/2001 dislike/3/2002 !!/5/2004 ?/6/2005)
+def tapback(message, emote):  # this is inline just for simplyfinging edits for futur ui changes (like/2/2001 dislike/3/2002 !!/5/2004 ?/6/2005)  # NOTE searches for first 100 chars of message
     d['title'] = sub(f""" osascript -e '
         tell application "System Events" to tell process "Messages"
-            set value of text field 1 of group 1 of group 1 of group 1 of group 1 of group 2 of group 1 of group 1 of group 1 of group 1 of group 1 of group 1 of group 1 of window 1 to \"{message}\"
+            set value of text field 1 of group 1 of group 1 of group 1 of group 1 of group 2 of group 1 of group 1 of group 1 of group 1 of group 1 of group 1 of group 1 of window 1 to \"{message[:100]}\"
             
-            delay 0.5    --to find the correct group hirachy just repeat with n from 1 to count of (entire contents of window 1 as list) log(get description/value/role of item n of (enire.. as list)) end repeat or use automator watch me do and copy steps to script editor
+            delay 1.0    --to find the correct group hirachy just repeat with n from 1 to count of (entire contents of window 1 as list) log(get description/value/role of item n of (enire.. as list)) end repeat or use automator watch me do and copy steps to script editor
             perform action "AXPress" of static text {"1 of button 1" if message.startswith('https://') else "2 of group 1 of group 3"} of group 1 of group 1 of group 1 of group 1 of group 1 of group 1 of group 2 of group 1 of group 1 of group 1 of group 2 of group 1 of group 1 of group 1 of group 1 of group 1 of group 1 of group 1 of window 1
             
-            delay 0.5    --find second or first group whose contains either message text or title of preview
+            delay 1.0    --find second or first group whose contains either message text or title of preview
             set previewormessage to {"description of static text 1 of button 1 of group 1 of group 1 of group 1 of group 1 of group 1 of group 1 of group 2 of group 1 of group 1 of group 1 of group 2 of group 1 of group 1 of group 1 of group 1 of group 1 of group 1 of group 1 of window 1" if message.startswith('https://') else '"'+message.replace('http://','').replace('https://','').split('/')[0]+'"' }
             perform action "AXShowMenu" of {"" if message.startswith("http") else "group 1 of"} (UI element 1 of group 1 of group 1 of group 1 of group 1 of group 3 of group 1 of group 1 of group 1 of group 1 of group 1 of group 1 of window 1 whose description contains previewormessage)
 
             perform action "AXPress" of menu item "{"Tapback…" if emote else "Delete…"}" of menu 1 of group 1 of window 1
-            delay 0.5    --this is slower than keystroke command t + 1 but works wihle messages is in background
+            delay 1.0    --this is slower than keystroke command t + 1 but works wihle messages is in background
             perform action "AXPress" of button {emote if emote else '"'+"Delete"+'"'} {"of group 1 of group 3 of group 1 of group 2 of group 1 of group 1" if emote else "of sheet 1"} of window 1
         
             log(previewormessage)    --print this for use as folder name
         end tell' """, True)
+    if "execution error" in d['title']: sub("""osascript -e 'quit app "Messages"' -e 'delay 2' -e 'tell application "Messages" to activate'""", True); sys.exit()  # tapback error makes messages restart and exits so nothing happes
 
-        # TODO would be nice to use this title as outdir name but witings needs to search outdir without running tapback befor
-        #   so find a way to search dirs wich end on date for mp4s inside /temps/tapbacktitle date/final.mp4 (attention to nested folders and files)
-        #   [f.path for f in os.scandir("/Users/mini/Desktop") if f.endswith("copy")]
-    
-
-def head():  # TODO adjust serach message.text length for tpaback message TODO perhaps to much tapbacks and need to sys.exit early # runs all for loops once so worst case cleanups.tapback(!!) + cleanups.tapback(delete) + todos.tapback(dislike) + waitings.tapback(like)
-    parsereadlist(); d['screens'](); d['locaway']()
+def head():  # TODO perhaps to much tapbacks and need to sys.exit early # runs all for loops once so worst case cleanups.tapback(!!) + cleanups.tapback(delete) + todos.tapback(dislike) + waitings.tapback(like)
+    parsereadlist(); d['screens'](); d['pingout'] = d['locaway']()   # since u cannot assign inside lambda but like to not ping multible times
 
     for panics in [m for m in d['sqllist'] if [l for l in d['sqllist'] if 2004 in l and any(str(s).startswith('to') for s in l)] or not d['pingout']]:  # savety list with all messages when there is a !! 'to' message (from me) so vpn wants off or actually is off
         if panics[3].startswith('http'):  sub(f'screen -X -S {panics[1]} quit', True)  # drop all dl screens
         if not d.get('sentpanic'):        sub(f"osascript -e 'tell application \"Messages\" to send \"dls but vpn off\" to participant \"{d['phonenr']}\"'", True); d['sentpanic'] = True  # sned panic only once
 
     for cleanups in [m for m in d['sqllist'] if 2004 in m]:
-        if cleanups[0].startswith('http'):                 sub(f'screen -X -S {cleanups[1]} quit', True); tapback(celanups[0], 5); tapback(cleanups[0], False); break  # http message and has !! (from me) - specific screen off
-        if cleanups[0].startswith('to') and d['pingout']:  sub(d['sshspinala'](todos[1], 'disconnect'), True); tapback(celanups[0], 5); tapback(cleanups[0], False); break  # to message and has !! (from me) and vpn currently on - vpn off
+        if cleanups[0].startswith('http'):                tapback(celanups[0], 5); tapback(cleanups[0], False); sub(f'screen -X -S {cleanups[1]} quit', True);      break  # http message and has !! (from me) - specific screen off
+        if cleanups[0].startswith('to') and d['pingout']: tapback(celanups[0], 5); tapback(cleanups[0], False); sub(d['sshspinala'](todos[1], 'disconnect'), True); break  # to message and has !! (from me) and vpn currently on - vpn off
 
     for todos in [m for m in d['sqllist'] if None in m]:  #TODO before if None in m istead of if not m[3] # vpn should be on top cause of sql sort
-        if todos[0].startswith('http') and d['pingout'] and d['screens']().count('(Detached)') < 6:  sub(f'mkdir {d["outdir"](todos[0], todos[1])} && cd {d["outdir"](todos[0], todos[1])} && screen -L -S {todos[1]} -d -m dl {todos[0]}', True); tapback(todos[0], 3); break  # message starts 'http' and has None tapback and vpn currently on and screen sockets less than 6 - dl on
-        if todos[0].startswith('to') and not d['pingout']:                                         sub(d['sshspinala'](todos[1], f'connect {todos[0][:-2]}'), True); tapback(todos[0], 3); break  # message starts 'to' and has None tapback and vpn currently off - vpn on
+        if todos[0].startswith('http') and d['pingout'] and d['screens']().count('(Detached)') < 6: tapback(todos[0], 3); sub(f'mkdir {d["outdir"](todos[1])} && cd {d["outdir"](todos[1])} && screen -L -S {todos[1]} -d -m dl {todos[0]}', True); break  # message starts 'http' and has None tapback and vpn currently on and screen sockets less than 6 - dl on
+        if todos[0].startswith('to')                    and not d['pingout']:                       tapback(todos[0], 3); sub(d['sshspinala'](todos[1], f'connect {todos[0][:-2]}'), True);                                                         break  # message starts 'to' and has None tapback and vpn currently off - vpn on
 
     for waitings in [m for m in d['sqllist'] if 2002 in m and m[1] not in d['screens']]:  # has dislike and has no active screen
-        tapback(waitings[0], 2) if waitings[0].startswith('http') and d['searchfiles'](d['files'](  d['outdir'](waitings[0], waitings(1)), 'mp4', 'mp4'  ), 'mp4') else tapback(waitings[0], 6); break  # message 'http....' has no screen and in outdir is mp4 -> tapback like else tapback ?
-        tapback(waitings[0], 2) if waitings[0].startswith('to') and d['pingout'] else tapback(waitings[0], 6); break  # message 'to....' has no screen and locaway is True so vpn ok -> tapback like else tapback ?
+        tapback(waitings[0], 2) if waitings[0].startswith('http') and d['searchfiles'](d['files'](  [f.path for f in os.scandir(d['outdir']) if f.path.endswith(waitings[1])][1], 'mp4', 'mp4'  ), 'mp4') else tapback(waitings[0], 6); break  # message 'http....' has no screen and in /outdir/subdir with end of m.date/ has mp4s -> tapback like else tapback ?
+        tapback(waitings[0], 2) if waitings[0].startswith('to')   and d['pingout']                                                                                                                        else tapback(waitings[0], 6); break  # message 'to....' has no screen and locaway is True so vpn ok -> tapback like else tapback ?
 
     print(d.get(sys.argv[3].strip("''"), len([m for m in d['sqllist'] if None in m]) ))  # print sth from dict for debugging or print count of urls as 'CurrentRelativeHumidity' to homebridge
 
@@ -102,13 +96,13 @@ def helps():
 
         /humidreadlist.py sort                  sorts '{os.getcwd()}/subd/file.notmp4s' to '{os.getcwd()}/subd/{' '.join(os.getcwd().split('/')[-1].split(' ')[:-1])}.mp4s'
 
-    """)  # NOTE perhaps actually list sort files and final files here
+    """)  # NOTE perhaps actually list sort files and predicted final files here
 
 
 d = {'get': head, 'dl': dl, 'sort': sort, # defs for running directly in cli via arguments
     'sshspinala':   lambda whereto, date:       f'screen -S {date} -d -m ssh spinala@192.168.2.1 -i {secs.minisshpriv} nordvpn {whereto}',
-    'outdir':       lambda message, date:       os.path.join('/Users/mini/Downloads/temps/', f'{message.split("/",3)[2].replace("."," ")} {date}'),  # TODO perhaps change temps to desktop dir # slpit() creates list like ['http:', '', 'final.file.whatever', 'url'] so mkdir to dl to makes ..../temp/final file whatever date/  # so naming convention is http://final.file.whatever/url
-    'locaway':      lambda:                     d['pingout'] = True if requests.get(f'http://ipinfo.io/json', timeout=2, verify=False).json().get('country', "DE").lower() != 'de' else False,  # everything but de will be treated as vpn on this is very bad here no https cause of error message
+    'outdir':       lambda date:                os.path.join('/Users/mini/Downloads/temps/', f'{d.get("title","")[:20].replace("."," ")}{" "+date if d.get("title") else ""}'),  # form http://final.file.whatever/url either makes /outdir/final file whatever date/ or just gives base /outdir to waitings to search /outdir/scan for subdir with date/ for existance of mp4s
+    'locaway':      lambda:                     True if requests.get(f'http://ipinfo.io/json', timeout=2, verify=False).json().get('country', "DE").lower() != 'de' else False,  # everything but de will be treated as vpn on this is very bad here no https cause of error message
     'screens':      lambda:                     sub('screen -list', True),  # sub returns cmd output as string and then in listcomp for waitings date is unique in string
     'files':        lambda filesdir, one, two:  [  sorted([f'{p}/{n}' for n in f if n.endswith(one) or n.endswith(two)])  for p, s, f in os.walk(filesdir)],  # [ ['dir1/file1.mkv', 'dir1/file2.srt'], ['dir2/file1.mkv', 'dir2/file2.srt']  ] but with a lot of empty lists if subdir has no matches
     'searchfiles':  lambda files, end:          [f for sl in files for f in sl if f.endswith(end)]  # [dir1/file2.srt, dir2/file2.srt]
